581736d3f83b880dba4a747363e6b12fe7d04740
Himanshu <himanshu620434@gmail.com>
2026-06-22 22:28:00 +0530

    feat: add user-service folder and initial implementation

---
+++ services/user-service/src/server.ts
@@ -0,0 +1,7 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.USER_PORT || 3003;
+
+app.listen(PORT, () => console.log(`🚀 User service running on port ${PORT}`));
